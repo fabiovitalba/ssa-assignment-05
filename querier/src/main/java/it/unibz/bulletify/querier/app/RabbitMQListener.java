ff2b745197b8a9daef15549b0e5073bf5f3ac5b8
Fabio.Vitalba <fabvit9@gmail.com>
2024-05-13 22:56:47 +0200

    Add implementation for MessageListener

--- querier/src/main/java/it/unibz/bulletify/querier/app/RabbitMQListener.java
+++ querier/src/main/java/it/unibz/bulletify/querier/app/RabbitMQListener.java
@@ -1,5 +1,7 @@
 package it.unibz.bulletify.querier.app;
 
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
@@ -18,5 +20,24 @@ public class RabbitMQListener {
         // do not quite match each other. We need to match the RabbitMQ interface (API)
         // to the code that we want to run, when there's a new message (=RecordItems).
 
+        // MessageListenerAdapter uses meta-programming in order to find the correct method
+        // to call using the provided string.
+        return new MessageListenerAdapter(recordItems,"recordItem");
+    }
+
+    @Bean
+    SimpleMessageListenerContainer container(ConnectionFactory connFactory, MessageListenerAdapter adapter) {
+        // This container is a package that we're going to inject into Spring, so that Spring
+        // knows what to do, when messages come in.
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+
+        // For the connection connFactory that we have:
+        container.setConnectionFactory(connFactory);
+        // it goes to the queue "items":
+        container.setQueueNames("items");
+        // attaches the listener for new messages:
+        container.setMessageListener(adapter);
+
+        return container;
     }
 }
