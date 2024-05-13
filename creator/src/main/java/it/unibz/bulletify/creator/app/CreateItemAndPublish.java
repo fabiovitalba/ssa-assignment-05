6e497e80012172eaad4936aff943c7e764b78172
Fabio.Vitalba <fabvit9@gmail.com>
2024-05-13 21:51:19 +0200

    Add new class and structure

---
+++ creator/src/main/java/it/unibz/bulletify/creator/app/CreateItemAndPublish.java
@@ -0,0 +1,10 @@
+package it.unibz.bulletify.creator.app;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateItemAndPublish {
+    // receive the data from the request
+    // publish a message to rabbitmq
+    // store in the database
+}
